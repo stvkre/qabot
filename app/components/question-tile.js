c6f9c849e9421727befef74ce95fd72ddfa4ce3c
stvkre <kiariemburuh@gmail.com>
2017-04-07 17:43:14 +0300

    editted question-tile.js to allow deployment

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -4,6 +4,6 @@ export default Ember.Component.extend({
   delete(rental) {
     if (confirm('Are you sure you want to delete this rental?')) {
       this.sendAction('destroyQuestion', question);
-    },
-  },
+    }
+  }
 });
